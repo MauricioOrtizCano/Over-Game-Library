4fef25cafb3fce375f783788b06ddf7eb4a056db
MauricioOrtizCano <cmoc14@yahoo.com>
2023-03-24 22:05:46 -0500

    arreglando el back

--- api/index.js
+++ api/index.js
@@ -17,31 +17,31 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn, Genre } = require('./src/db.js');
-const axios = require('axios');
-const { API_KEY } = process.env;
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+require("dotenv").config();
+//const axios = require("axios");
+const { PORT } = process.env;
 
-function genresToDB() {
-  axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`).then(r => {
-      let arrGenre = r.data.results.map(g => {
-          const genre = {
-                  id: g.id,
-                  name: g.name,
-                  games: g.games.map(vg => vg.name)
-              };
-              return genre;
-      })
+// function genresToDB() {
+//   axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`).then((r) => {
+//     let arrGenre = r.data.results.map((g) => {
+//       const genre = {
+//         id: g.id,
+//         name: g.name,
+//         games: g.games.map((vg) => vg.name),
+//       };
+//       return genre;
+//     });
 
-      Genre.bulkCreate(arrGenre)
-  })
-}
-
+//     Genre.bulkCreate(arrGenre);
+//   });
+// }
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(async() => {
-  await genresToDB()
-  server.listen(3001, () => {
-    console.log('Listening at 3001'); // eslint-disable-line no-console
+conn.sync({ force: true }).then(() => {
+  //await genresToDB();
+  server.listen(PORT, () => {
+    console.log(`Listening at ${PORT} port`); // eslint-disable-line no-console
   });
 });
