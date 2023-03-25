6f8b80b10cc160ee8c7b008cf273d83d08ea3c27
MauricioOrtizCano <cmoc14@yahoo.com>
2023-03-24 22:11:47 -0500

    Agregando el config env

--- api/src/db.js
+++ api/src/db.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
+require("dotenv").config();
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
